aef6a14d252935c065c2c2e2296605601f823de9
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2025-05-26 02:47:13 +0900

    feat: add try-catch blocks

--- src/main/java/com/totwo/chat/controller/FriendController.java
+++ src/main/java/com/totwo/chat/controller/FriendController.java
@@ -1,6 +1,7 @@
 package com.totwo.chat.controller;
 
 import com.totwo.chat.common.CommonResponse;
+import com.totwo.chat.common.CustomError;
 import com.totwo.chat.dto.UserDto;
 import com.totwo.chat.security.AuthUtils;
 import com.totwo.chat.service.FriendService;
@@ -21,18 +22,26 @@ public class FriendController {
 
     @GetMapping("/list/get")
     public ResponseEntity<CommonResponse<List<UserDto>>> getFriendList() {
-        String email = authUtils.getEmail();
-        List<UserDto> userList = friendService.getFriends(email);
-        log.info("[api /friend/list/get]: {}", userList);
-        return CommonResponse.ok(userList);
+        try {
+            String email = authUtils.getEmail();
+            List<UserDto> userList = friendService.getFriends(email);
+            log.info("[api /friend/list/get]: {}", userList);
+            return CommonResponse.ok(userList);
+        } catch (Exception e) {
+            return CommonResponse.failed(new CustomError("00000", e.getMessage()));
+        }
     }
 
     @PostMapping("/private/add")
     public ResponseEntity<CommonResponse<UserDto>> addFriend(@RequestBody UserDto userDto) {
-        String myEmail = authUtils.getEmail();
-        String friendEmail = userDto.getEmail();
-        friendService.registerFriend(myEmail, friendEmail);
-        log.info("[api /friend/private/add]");
-        return CommonResponse.created(null);
+        try {
+            String myEmail = authUtils.getEmail();
+            String friendEmail = userDto.getEmail();
+            friendService.registerFriend(myEmail, friendEmail);
+            log.info("[api /friend/private/add]");
+            return CommonResponse.created(null);
+        } catch (Exception e) {
+            return CommonResponse.failed(new CustomError("00001", e.getMessage()));
+        }
     }
 }
