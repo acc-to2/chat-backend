ae5c1336001156e54e7c1d7b724886dbc661aada
Ssomewhat <isc10120@gmail.com>
2025-05-27 19:59:35 +0900

    hotfix

--- src/main/java/com/totwo/chat/controller/ChatSocketController.java
+++ src/main/java/com/totwo/chat/controller/ChatSocketController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
@@ -21,18 +22,20 @@ public class ChatSocketController {
     private final MessageService messageService;
 
     @MessageMapping("/{room_id}/in")
-    public void join(@DestinationVariable String roomId) {
+    public void join(@DestinationVariable("room_id") String roomId) {
         log.info("User in room: {}", roomId);
     }
 
     @MessageMapping("/{room_id}/out")
-    public void leave(@DestinationVariable String roomId) {
+    public void leave(@DestinationVariable("room_id") String roomId) {
         log.info("User left room: {}", roomId);
     }
 
     @MessageMapping("/{room_id}/send")
-    public void send(@DestinationVariable String roomId, ChatMessage message) {
+    @SendTo("/{room_id}/in")
+    public void send(@DestinationVariable("room_id") String roomId, ChatMessage message) {
         message.setRoomId(roomId);
+        log.info("User in subscribe room: {}", roomId);
         message.setType("SEND");
         message.setTimestamp(LocalDateTime.now());
         messageService.saveMessage(
@@ -41,6 +44,5 @@ public class ChatSocketController {
                 message.getContent(),
                 String.valueOf(message.getTimestamp().atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli())
         );
-        publisher.publish(message);
     }
 }
