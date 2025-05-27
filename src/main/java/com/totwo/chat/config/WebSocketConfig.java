9499b7b062b0411f0aebd8a4ba8f16c94985f555
Ssomewhat <isc10120@gmail.com>
2025-05-27 17:24:04 +0900

    fix: 웹소켓 mq연결, SockJS 설정

--- src/main/java/com/totwo/chat/config/WebSocketConfig.java
+++ src/main/java/com/totwo/chat/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.totwo.chat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -13,12 +14,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat") // 클라이언트가 연결할 WebSocket endpoint
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // SockJS fallback
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/chat"); // 클라이언트 → 서버 메시지 전송 prefix
-        registry.enableSimpleBroker("/topic"); // 서버 → 클라이언트 브로드캐스트 prefix
+        registry.enableStompBrokerRelay("/topic"); // 서버 → 클라이언트 브로드캐스트 prefix
     }
 }
